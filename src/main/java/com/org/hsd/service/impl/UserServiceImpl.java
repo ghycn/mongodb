a8d56a387188f42e6b2dd3ecbb8de2080c8b0034
ghycn <349111145@qq.com>
2017-09-09 23:34:19 +0800

    mongo条件查询

--- src/main/java/com/org/hsd/service/impl/UserServiceImpl.java
+++ src/main/java/com/org/hsd/service/impl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.org.hsd.service.impl;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.org.hsd.model.Student;
 import com.org.hsd.model.User;
 import com.org.hsd.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -31,8 +38,14 @@ public class UserServiceImpl implements IUserService {
 
     }
 
-    public User queryUser() {
-      return   mongoTemplate.findById("586b4e620659e85742ab5d9b",User.class);
+    public User queryUser() { 
+        DBCollection collection = mongoTemplate.getCollection("student");
+        Criteria  criteria= Criteria.where("name").is("jack");
+        Query query = Query.query(criteria);
+        List<Student> list = mongoTemplate.find(query,Student.class);
+
+
+        return   mongoTemplate.findById("586b4e620659e85742ab5d9b",User.class);
 
     }
 
